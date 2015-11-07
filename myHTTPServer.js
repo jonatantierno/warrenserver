7e1de801bb8d69fd6420295805ef3977b1ec8186
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2015-11-07 07:39:11 +0100

    changes

--- myHTTPServer.js
+++ myHTTPServer.js
@@ -1,15 +1,19 @@
 //Lets require/import the HTTP module
 var http = require('http');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var money = 0;
 var coins = 0;
 var shareCoefficient = 1;
+var company = "fb";
 
 //Lets define a port we want to listen to
 const PORT=9559;
 
 var express = require('express');
 var app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
@@ -18,15 +22,18 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-
-app.get('/money/', function (req, res) {
+app.get('/money', function (req, res) {
   money = coins*shareCoefficient;
   res.send({value: money});
-
 });
 
-app.get('/stock/', function (req, res) {
-  res.send({value: shareCoefficient});
+app.post('/company', function (req, res) {
+  company = req.body.company;
+  res.send("OK");
+});
+
+app.get('/stock', function (req, res) {
+  res.send({value: coins, coeficient: shareCoefficient, selectedCompany: company});
 });
 
 app.post('/stock', function (req, res) {
@@ -36,14 +43,16 @@ app.post('/stock', function (req, res) {
 
 });
 
-app.get('/coin/', function (req, res) {
+app.get('/coin', function (req, res) {
   res.send({value: coins});
-
 });
 
-app.post('/coin/', function (req, res) {
+app.post('/coin', function (req, res) {
   coins = coins+1;
-  res.send({value: coins});
+  console.log('coin added ', coins);
+  res.send({value: coins, coeficient: shareCoefficient, selectedCompany: company});
+
+  // notify GCM message
 });
 
 
